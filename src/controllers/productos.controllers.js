f474ec4301018376b6303d67e72a9e66591de05a
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-28 18:36:55 -0300

    se dió de alta el producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -4,8 +4,13 @@ export const controladorPrueba = (req, res) => {
   res.send('Esta es una prueba de mi ruta GET de prueba');
 };
 
-export const crearProducto = (req, res) => {
+export const crearProducto = async (req, res) => {
   try {
+    const productoNuevo = new Producto(req.body);
+    await productoNuevo.save();
+    res.status(201).json({
+      message: 'El producto fue creado correctamente.',
+    });
   } catch (error) {
     console.log(error);
     res.status(404).json({
